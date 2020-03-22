e52c9cec19beae2bbac75a6d8966076c18eec832
Ilya Kisil <ilyakisil@gmail.com>
2020-03-22 19:52:32 +0000

    fix(ssh): additionally generate ssh keys from local config if they specified

--- setup-utils/common/setup-ssh.sh
+++ setup-utils/common/setup-ssh.sh
@@ -4,31 +4,34 @@ set -ue
 
 function main() {
     local SSH_DIR=${HOME}/.ssh
-    local SSH_CONFIG=${SSH_DIR}/config
     local SSH_USER=`whoami`
     local SSH_PC=`uname -n`
+    local SSH_CONFIGS
+    declare -a SSH_CONFIGS=("${SSH_DIR}/config"
+                            "${SSH_DIR}/config-local"
+                            )
 
-    if [[ -d ${SSH_DIR} ]] && [[ -f ${SSH_CONFIG} ]]; then
-        echo '==> Creating ssh keys'
-
-        for LINE in $(grep "IdentityFile" ${SSH_CONFIG});
-        do
-            # This check is a way around of grep automatically splitting
-            # by space a line with a match. So we just disregard those elements
-            if [[ ${LINE} != "IdentityFile" ]]; then
-                ssh_key_name=`echo ${LINE} | awk -F '/' '{ print $NF }'`
-                ssh_key_path="${SSH_DIR}/${ssh_key_name}"
-                if [ ! -f ${ssh_key_path} ] && [ ! -f "${ssh_key_path}.pub" ]   ; then
-                    printf "=> Creating [${ssh_key_path}] SSH key.\n"
-                    ssh-keygen -t rsa -b 4096 -C "${SSH_USER}@${SSH_PC}" -f ${ssh_key_path} -q -N ""
-                else
-                    printf "=> SSH key [${ssh_key_path}] exists. Creation is skipped.\n"
+    for ssh_config in ${SSH_CONFIGS[@]}; do
+        if [[ -d ${SSH_DIR} ]] && [[ -f ${ssh_config} ]]; then
+            for LINE in $(grep "IdentityFile" ${ssh_config});
+            do
+                # This check is a way around of grep automatically splitting
+                # by space a line with a match. So we just disregard those elements
+                if [[ ${LINE} != "IdentityFile" ]]; then
+                    ssh_key_name=`echo ${LINE} | awk -F '/' '{ print $NF }'`
+                    ssh_key_path="${SSH_DIR}/${ssh_key_name}"
+                    if [ ! -f ${ssh_key_path} ] && [ ! -f "${ssh_key_path}.pub" ]   ; then
+                        printf "=> Creating [${ssh_key_path}] SSH key.\n"
+                        ssh-keygen -t rsa -b 4096 -C "${SSH_USER}@${SSH_PC}" -f ${ssh_key_path} -q -N ""
+                    else
+                        printf "=> SSH key [${ssh_key_path}] exists. Creation is skipped.\n"
+                    fi
                 fi
-            fi
-        done
-    else
-        echo "==> SSH config [~/.ssh/config] file do not exists at user level."
-    fi
+            done
+        else
+            echo "=> SSH config [${ssh_config}] file do not exists at user level."
+        fi
+    done
 }
 
 main
