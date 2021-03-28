f3f647550aca714a35f9830b3f50076b79e6bc7a
Ilya Kisil <ilyakisil@gmail.com>
2021-03-28 02:57:11 +0100

    feat(nvim): handy functions for hot reload

---
+++ dotfiles/nvim/lua/ik1614/globals/init.lua
@@ -0,0 +1,20 @@
+-- References:
+-- * https://github.com/tjdevries/config_manager/blob/627eb44c5324b9f6f0014abb995f8b29b548b7d7/xdg_config/nvim/lua/tj/globals/init.lua
+-- * Some where in this video TJ DeVries goes over about this stuff https://www.youtube.com/watch?v=u6EKq6z0CRU
+
+-- Utility to print stuff
+P = function(v)
+  print(vim.inspect(v))
+  return v
+end
+
+
+-- Utility to hot reload/require stuff
+if pcall(require, 'plenary') then
+  RELOAD = require('plenary.reload').reload_module
+
+  R = function(name)
+    RELOAD(name)
+    return require(name)
+  end
+end
