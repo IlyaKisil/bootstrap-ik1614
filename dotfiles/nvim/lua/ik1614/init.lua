3a70216229fbef6b93b315a583d2ecdf85083321
Ilya Kisil <ilyakisil@gmail.com>
2021-04-17 01:16:26 +0100

    feat(nvim): initial setup for bash lsp

--- dotfiles/nvim/lua/ik1614/init.lua
+++ dotfiles/nvim/lua/ik1614/init.lua
@@ -29,7 +29,7 @@ require("ik1614.lsp")
 require("ik1614.lsp.typescript")
 require("ik1614.lsp.lua")
 -- require("ik1614.lsp.python")
--- require("ik1614.lsp.bash")
+require("ik1614.lsp.bash")
 -- require("ik1614.lsp.terraform")
 
 
