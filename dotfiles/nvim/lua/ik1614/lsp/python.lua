82a95aecfd3d5e04b8e783416d580280978e6d23
Ilya Kisil <ilyakisil@gmail.com>
2021-04-18 23:44:23 +0100

    feat(nvim): initial setup for python LSP

--- dotfiles/nvim/lua/ik1614/lsp/python.lua
+++ dotfiles/nvim/lua/ik1614/lsp/python.lua
@@ -1,7 +1,8 @@
 -- npm i -g pyright
+-- LspInstall python
 
 require'lspconfig'.pyright.setup {
-    -- cmd = {DATA_PATH .. "/lspinstall/python/node_modules/.bin/pyright-langserver", "--stdio"},
+    cmd = {DATA_PATH .. "/lspinstall/python/node_modules/.bin/pyright-langserver", "--stdio"},
     on_attach = require'ik1614.lsp'.common_on_attach,
     handlers = {
         ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
@@ -9,7 +10,6 @@ require'lspconfig'.pyright.setup {
             signs = O.python.diagnostics.signs,
             underline = O.python.diagnostics.underline,
             update_in_insert = true
-
         })
     }
 }
