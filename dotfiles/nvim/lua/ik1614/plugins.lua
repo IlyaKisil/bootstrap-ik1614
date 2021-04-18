6f62c6c0fbb8f23637dee533122b08db4b2b3d17
Ilya Kisil <ilyakisil@gmail.com>
2021-04-18 22:09:39 +0100

    feat(nvim): treesitter text objects plugin

--- dotfiles/nvim/lua/ik1614/plugins.lua
+++ dotfiles/nvim/lua/ik1614/plugins.lua
@@ -40,6 +40,7 @@ return require('packer').startup(
 
     -- Treesitter
     use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+    use {'nvim-treesitter/nvim-treesitter-textobjects'}
     -- use 'nvim-treesitter/nvim-treesitter-refactor'
     use 'nvim-treesitter/playground'
 
--- dotfiles/nvim/lua/ik1614/treesitter/init.lua
+++ dotfiles/nvim/lua/ik1614/treesitter/init.lua
@@ -46,6 +46,17 @@ require'nvim-treesitter.configs'.setup {
   indent = {
     enable = true
   },
+  textobjects = {
+    select = {
+      enable = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
+    },
+  },
   -- autotag = {
   --     enable = true
   -- },
