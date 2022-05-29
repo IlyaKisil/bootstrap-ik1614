f36727265b44eeb5b5841c815fa02498321e4e5c
Ilya Kisil <ilyakisil@gmail.com>
2022-05-30 00:30:23 +0100

    chore(nvim/fzf-lua): exlude pattern for `rg`

--- dotfiles/nvim/lua/ik1614/fzf-lua/init.lua
+++ dotfiles/nvim/lua/ik1614/fzf-lua/init.lua
@@ -158,7 +158,7 @@ fzf.setup {
     -- otherwise auto-detect prioritizes `rg` over `grep`
     -- default options are controlled by 'rg|grep_opts'
     -- cmd            = "rg --vimgrep",
-    rg_opts           = "--column --line-number --no-heading --color=always --smart-case --max-columns=512",
+    rg_opts           = "--column --line-number --no-heading --color=always --smart-case --max-columns=512 --glob '!package-lock.json'",
     grep_opts         = "--binary-files=without-match --line-number --recursive --color=auto --perl-regexp",
     -- 'live_grep_glob' options:
     glob_flag         = "--iglob",  -- for case sensitive globs use '--glob'
