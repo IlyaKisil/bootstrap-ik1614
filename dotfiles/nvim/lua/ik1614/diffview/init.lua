d4d25106416c1999ecdf5f13972178539f379619
Ilya Kisil <ilyakisil@gmail.com>
2022-07-04 01:36:28 +0100

    chore(diffview): update as per https://github.com/sindrets/diffview.nvim/pull/136

--- dotfiles/nvim/lua/ik1614/diffview/init.lua
+++ dotfiles/nvim/lua/ik1614/diffview/init.lua
@@ -3,20 +3,24 @@ local cb = require('diffview.config').diffview_callback
 require('diffview').setup {
   use_icons = true,         -- Requires nvim-web-devicons
   file_panel = {
-    position = "bottom",                  -- One of 'left', 'right', 'top', 'bottom'
-    height = 10,                        -- Only applies when position is 'top' or 'bottom'
-    listing_style = "list",             -- One of 'list' or 'tree'
+    win_config = {
+      position = "bottom",                  -- One of 'left', 'right', 'top', 'bottom'
+      height = 10,                        -- Only applies when position is 'top' or 'bottom'
+      listing_style = "list",             -- One of 'list' or 'tree'
+    },
   },
   file_history_panel = {
-    position = "bottom",
-    height = 10,
-    log_options = {
-      max_count = 256,      -- Limit the number of commits
-      follow = false,       -- Follow renames (only for single file)
-      all = false,          -- Include all refs under 'refs/' including HEAD
-      merges = false,       -- List only merge commits
-      no_merges = false,    -- List no merge commits
-      reverse = false,      -- List commits in reverse order
+    win_config = {
+      position = "bottom",
+      height = 10,
+      log_options = {
+        max_count = 256,      -- Limit the number of commits
+        follow = false,       -- Follow renames (only for single file)
+        all = false,          -- Include all refs under 'refs/' including HEAD
+        merges = false,       -- List only merge commits
+        no_merges = false,    -- List no merge commits
+        reverse = false,      -- List commits in reverse order
+      },
     },
   },
   default_args = {    -- Default args prepended to the arg-list for the listed commands
@@ -29,7 +33,9 @@ require('diffview').setup {
     -- tabpage is a Diffview.
     view = {
       ["<tab>"]      = cb("select_next_entry"),  -- Open the diff for the next file
+      ["<C-n>"]      = cb("select_next_entry"),  -- Open the diff for the next file
       ["<s-tab>"]    = cb("select_prev_entry"),  -- Open the diff for the previous file
+      ["<C-e>"]      = cb("select_prev_entry"),  -- Open the diff for the previous file
       ["gf"]         = cb("goto_file"),          -- Open the file in a new split in previous tabpage
       ["<C-w><C-f>"] = cb("goto_file_split"),    -- Open the file in a new split
       ["<C-w>gf"]    = cb("goto_file_tab"),      -- Open the file in a new tabpage
@@ -45,7 +51,9 @@ require('diffview').setup {
       -- ["X"]             = cb("restore_entry"),        -- Restore entry to the state on the left side.
       ["r"]             = cb("refresh_files"),        -- Update stats and entries in the file list.
       ["<tab>"]         = cb("select_next_entry"),
+      ["<C-n>"]         = cb("select_next_entry"),
       ["<s-tab>"]       = cb("select_prev_entry"),
+      ["<C-e>"]         = cb("select_prev_entry"),
       ["gf"]            = cb("goto_file"),
       ["<C-w><C-f>"]    = cb("goto_file_split"),
       ["<C-w>gf"]       = cb("goto_file_tab"),
