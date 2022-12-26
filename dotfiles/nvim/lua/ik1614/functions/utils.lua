2d877789575e66e702ac88c53b8ba2dfb75651d5
Ilya Kisil <ilyakisil@gmail.com>
2022-12-26 01:41:51 +0000

    refactor(nvim): move more `utils` functions to a new home :wink:

--- dotfiles/nvim/lua/ik1614/functions/utils.lua
+++ dotfiles/nvim/lua/ik1614/functions/utils.lua
@@ -53,4 +53,32 @@ function M:plugin_installed(name)
   return true
 end
 
+-- Merge two tables without mutation
+function M:table_merge(t1, t2)
+  local result = {}
+
+  for _, v in ipairs(t1) do
+    table.insert(result, v)
+  end
+
+  for _, v in ipairs(t2) do
+    table.insert(result, v)
+  end
+
+  return result
+end
+
+function M:table_to_string(t, separator)
+  local result = ""
+
+  for _, v in pairs(t) do
+      result = result .. separator .. v
+  end
+
+  -- Remove first hanging separtor
+  result = result:sub(2)
+
+  return result
+end
+
 return M.new()
