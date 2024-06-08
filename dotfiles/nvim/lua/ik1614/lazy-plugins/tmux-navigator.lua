2841a8a7ccfc93e89aa8c72b5566d916502b2a57
Ilya Kisil <ilyakisil@gmail.com>
2024-06-08 20:25:47 +0100

    chore(nvim): port `tmux-navigator` config into `lazy.nvim`

--- dotfiles/nvim/lua/ik1614/lazy-plugins/tmux-navigator.lua
+++ dotfiles/nvim/lua/ik1614/lazy-plugins/tmux-navigator.lua
@@ -1,1 +1,26 @@
-return {}
+return {
+  {
+    "https://github.com/numToStr/Navigator.nvim",
+    enabled = true,
+    config = function()
+
+      require("Navigator").setup({
+        auto_save = 'all',
+        disable_on_zoom = true
+      })
+
+      local f = require("ik1614.functions")
+      f.mapping:n({"<S-Left>",  '<CMD>NavigatorLeft<CR>'})
+      f.mapping:n({"<S-Right>", '<CMD>NavigatorRight<CR>'})
+      f.mapping:n({"<S-Up>",    '<CMD>NavigatorUp<CR>'})
+      f.mapping:n({"<S-Down>",  '<CMD>NavigatorDown<CR>'})
+      f.mapping:n({"<C-\\>",    '<CMD>NavigatorPrevious<CR>'})
+
+      f.mapping:i({"<S-Left>",  '<C-c><CMD>NavigatorLeft<CR>'})
+      f.mapping:i({"<S-Right>", '<C-c><CMD>NavigatorRight<CR>'})
+      f.mapping:i({"<S-Up>",    '<C-c><CMD>NavigatorUp<CR>'})
+      f.mapping:i({"<S-Down>",  '<C-c><CMD>NavigatorDown<CR>'})
+      f.mapping:i({"<C-\\>",    '<C-c><CMD>NavigatorPrevious<CR>'})
+    end
+  }
+}
