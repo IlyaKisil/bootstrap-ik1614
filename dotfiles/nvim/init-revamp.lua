d9efe28c43ebb1589a1e9d116b71082ec29cd568
Ilya Kisil <ilyakisil@gmail.com>
2024-06-08 18:43:41 +0100

    chore(nvim): initial setup for switching to `lazy.nvim`

---
+++ dotfiles/nvim/init-revamp.lua
@@ -0,0 +1,9 @@
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+require('ik1614.globals')
+require('ik1614.globals.settings')
+require('ik1614.lazy')
+
+
