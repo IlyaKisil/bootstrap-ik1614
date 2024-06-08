df382e39eaf07036376da41272214572e6f6408f
Ilya Kisil <ilyakisil@gmail.com>
2024-06-08 20:03:57 +0100

    fixup! chore(nvim): initial setup for switching to `lazy.nvim`

--- dotfiles/nvim/lua/ik1614/snippets/ft/lua.lua
+++ dotfiles/nvim/lua/ik1614/snippets/ft/lua.lua
@@ -6,15 +6,17 @@ return {
   s(
       {
          trig = "plugin-setup",
-         dscr = "Add boiler plate for setting up a plugin with 'lazy.nvim'"
+         dscr = "Add boilerplate for setting up a plugin with 'lazy.nvim'"
       },
       fmt([[
         return {
           {
             "__URL_FILL_ME__",
             enabled = true,
+            event = {},
+            dependencies = {},
             config = function()
-              require("___").setup({})
+              require("__FILL_ME__").setup({})
             end
           }
         }
