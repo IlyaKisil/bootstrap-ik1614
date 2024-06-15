66a505bc62ef3ef142e2dbf29ab11a7804c047a6
Ilya Kisil <ilyakisil@gmail.com>
2024-06-15 17:45:52 +0100

    fixup! chore(nvim): port `nvim-cmp` config/setup into `lazy.nvim`

--- dotfiles/nvim/lua/ik1614/lazy-plugins/completion.lua
+++ dotfiles/nvim/lua/ik1614/lazy-plugins/completion.lua
@@ -27,49 +27,48 @@ return {
       }
 
       local default_mappings = {
-        ["<C-Space>"] = cmp.mapping.complete(), -- Manually trigger completion suggestions. Generally don't need this.
-        ["<C-a>"] = cmp.mapping.abort(), -- Close completion options
-        ["<C-c>"] = cmp.mapping.close(), -- Stop completion and go back to the original text
+        -- Manually trigger completion suggestions. Generally don't need this.
+        ["<C-Space>"] = cmp.mapping.complete(),
+
+        -- Close completion options
+        ["<C-a>"] = cmp.mapping.abort(),
+
+        -- Stop completion and go back to the original text
+        ["<C-c>"] = cmp.mapping.close(),
+
+        -- Move to [N]ext item in completion menue
+        ["<C-n>"] = cmp.mapping(
+          cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+          {"i", "c"}
+        ),
+
+        -- Move to previous item in completion menu
         ["<C-e>"] = cmp.mapping(
           cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
           {"i", "c"}
         ),
 
-        ["<C-n>"] = cmp.mapping(
-          cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-          {"i", "c"}
+        -- Scrolling [U]p / back
+        ['<C-u>'] = cmp.mapping(
+          cmp.mapping.scroll_docs(-4),
+          {"i"}
         ),
-        ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Scrolling back
-        ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Scrolling forward
 
-        -- Alternative could be using '<CR>'
-        ["<C-y>"] = cmp.mapping(
+        -- Scrolling [D]own / forward
+        ['<C-d>'] = cmp.mapping(
+          cmp.mapping.scroll_docs(4),
+          {"i"}
+        ),
+
+        -- Confirm selection
+        -- Alternative could be using '<C-y>' -> Confirm [Y]es
+        ["<CR>"] = cmp.mapping(
           cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Insert,
             -- behavior = cmp.ConfirmBehavior.Replace,
             select = true,
           },
           { "i", "c" }
-        ),
-
-        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-        -- * https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-        -- Alternative could be using Tab and S-Tab
-        ['<C-i>'] = cmp.mapping(
-          function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end,
-          { 'i', 's' }
-        ),
-        ['<C-h>'] = cmp.mapping(
-          function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end,
-          { 'i', 's' }
         ),
       }
 
