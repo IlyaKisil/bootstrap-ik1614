22b4fbe9a4a3e5a0e84ced3693544c452ad2a36e
Ilya Kisil <ilyakisil@gmail.com>
2024-06-22 13:14:34 +0100

    chore(nvim): enable custom `autocmd` for the new setup

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -4,8 +4,6 @@ require("ik1614.core.mappings")
 require("ik1614.core.vim-backport")
 
 require('ik1614.lazy')
-
--- Setup all autocommands
--- require("ik1614.autocmd")
+require("ik1614.autocmd")
 
 vim.cmd('luafile ~/.config/nvim/init-local.lua')
