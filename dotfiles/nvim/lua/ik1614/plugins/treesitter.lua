d26f6fe704f44ae9d6211b451576970e4c4afcc0
Ilya Kisil <ilyakisil@gmail.com>
2024-06-23 16:09:01 +0100

    chore(nvim): enable selection with Treesitter

--- dotfiles/nvim/lua/ik1614/plugins/treesitter.lua
+++ dotfiles/nvim/lua/ik1614/plugins/treesitter.lua
@@ -55,6 +55,15 @@ return {
         indent = {
           enable = true,
         },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-Space>",
+            node_incremental = "<C-Space>",
+            scope_incremental = "<CR>",
+            node_decremental = "<BS>",
+          },
+        },
       })
     end,
   },
