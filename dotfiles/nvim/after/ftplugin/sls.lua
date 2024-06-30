844a004a78aff8173f7a68a4acb5919690e508d2
Ilya Kisil <ilyakisil@gmail.com>
2024-06-30 19:49:54 +0100

    chore(nvim): convert `ftplugin` definitions to `lua` and keep only what's needed

---
+++ dotfiles/nvim/after/ftplugin/sls.lua
@@ -0,0 +1,6 @@
+vim.cmd([[
+  call functions#add_file_header([
+              \ '## -*- coding: utf-8 -*-',
+              \ "## vim" . ": ft=sls softtabstop=2 tabstop=2 shiftwidth=2 expandtab autoindent cc=90",
+              \ ])
+]])
