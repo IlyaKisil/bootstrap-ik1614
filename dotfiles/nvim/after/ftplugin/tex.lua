17854c0d02956414f3f6668fdffbe3c9b39a94f2
Ilya Kisil <ilyakisil@gmail.com>
2024-11-23 15:04:40 +0000

    chore: minor update of things that I had for a while

--- dotfiles/nvim/after/ftplugin/tex.lua
+++ dotfiles/nvim/after/ftplugin/tex.lua
@@ -5,11 +5,11 @@ vim.opt_local.wrap = true
 vim.cmd([[
   call functions#add_file_header([
               \ '% !TEX root =',
-              \ '% cSpell:ignoreRegExp ^[\s]*%.*',
-              \ '% cSpell:ignoreRegExp \\[0-9a-zA-Z]*',
-              \ '% cSpell:ignoreRegExp .*\\usepackage.*',
-              \ '% cSpell:ignoreRegExp .*\\cite\{[\w:\-,\s]*\}',
-              \ '% cSpell:ignoreRegExp .*\\c?ref\{[\w:\-]*\}',
-              \ '% cSpell:ignoreRegExp .*\\(begin|end){.*}.*',
+              "\ '% cSpell:ignoreRegExp ^[\s]*%.*',
+              "\ '% cSpell:ignoreRegExp \\[0-9a-zA-Z]*',
+              "\ '% cSpell:ignoreRegExp .*\\usepackage.*',
+              "\ '% cSpell:ignoreRegExp .*\\cite\{[\w:\-,\s]*\}',
+              "\ '% cSpell:ignoreRegExp .*\\c?ref\{[\w:\-]*\}',
+              "\ '% cSpell:ignoreRegExp .*\\(begin|end){.*}.*',
               \ ])
 ]])
