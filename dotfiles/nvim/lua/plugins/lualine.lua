fbe0a693ba869130a6bee26e11a5b31e60a08efe
Ilya Kisil <ilyakisil@gmail.com>
2025-05-24 17:06:03 +0100

    chore(nvim/lualine): display file info in winbar for every open split

---
+++ dotfiles/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,98 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      local function lsp_status(msg)
+        --[[
+            References:
+            * https://github.com/alpha2phi/neovim-for-beginner/blob/14-null-ls/lua/config/lualine.lua
+            * https://github.com/alpha2phi/neovim-for-beginner/blob/383bf9a7b655ef0da604e88773940a636ee3fae4/lua/config/lualine.lua#L22
+          --]]
+        msg = msg or ""
+        local cur_buf = 0
+
+        local info = {}
+        local buf_clients = vim.lsp.get_clients({ bufnr = cur_buf })
+
+        if next(buf_clients) == nil then
+          if type(msg) == "boolean" or #msg == 0 then
+            return ""
+          end
+          return msg
+        end
+
+        local lsp_info = {}
+        for _, client in pairs(buf_clients) do
+          table.insert(lsp_info, client.name)
+        end
+
+        if next(lsp_info) ~= nil then
+          table.insert(info, "LSP: " .. table.concat(lsp_info, ", "))
+        end
+
+        if next(info) == nil then
+          return ""
+        end
+
+        return table.concat(info, " | ")
+      end
+
+      local empty_section = {}
+
+      opts.options.disabled_filetypes.winbar = { "snacks_dashboard", "snacks_picker_list" }
+      opts.sections.lualine_a = {
+        {
+          "mode",
+          fmt = function(str)
+            return str:sub(1, 1)
+          end,
+          padding = {
+            left = 1,
+            right = 1,
+          },
+        },
+      }
+
+      opts.winbar = {
+        lualine_a = {
+          {
+            "filename",
+            path = 1,
+          },
+        },
+        lualine_b = empty_section,
+        lualine_c = empty_section,
+        lualine_x = empty_section,
+        lualine_y = {
+          { lsp_status },
+        },
+        lualine_z = {
+          {
+            "filetype",
+            fmt = function(str)
+              return "ft=" .. str
+            end,
+            icons_enabled = false,
+            padding = {
+              left = 0,
+              right = 1,
+            },
+          },
+        },
+      }
+      opts.inactive_winbar = {
+        lualine_a = empty_section,
+        lualine_b = {
+          {
+            "filename",
+            path = 1,
+          },
+        },
+        lualine_c = empty_section,
+        lualine_x = empty_section,
+        lualine_y = empty_section,
+        lualine_z = empty_section,
+      }
+    end,
+  },
+}
