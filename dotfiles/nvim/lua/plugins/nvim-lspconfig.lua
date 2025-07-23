294d436ac66d935c3d33999c0f3eb6bfaa1e3305
Ilya Kisil <ilyakisil@gmail.com>
2025-07-23 12:37:32 +0100

    chore(nvim/lsp): load `rachartier/tiny-inline-diagnostic.nvim` on `LspAttach` otherwise it doesn't get loaded when launching as `nvim .`

--- dotfiles/nvim/lua/plugins/nvim-lspconfig.lua
+++ dotfiles/nvim/lua/plugins/nvim-lspconfig.lua
@@ -27,7 +27,7 @@ return {
   },
   {
     "rachartier/tiny-inline-diagnostic.nvim",
-    event = "VeryLazy", -- Or `LspAttach`
+    event = "LspAttach",
     priority = 1000, -- needs to be loaded in first
     config = function()
       require("tiny-inline-diagnostic").setup({
